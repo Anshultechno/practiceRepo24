55b746234bee78508278ff3579c2a5097b3ad662
Anshul Ratner <ratneransh@gmail.com>
2024-06-27 20:28:22 +0530

    done inheritance

---
+++ src/inheritance/Child.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+	public class Child extends Parent{
+
+		int y =20;
+		int z =30;
+		
+		void m2() {
+			System.out.println("Child m2");
+		}
+		
+		void m3() {
+			System.out.println("Child m3");
+		}
+}
---
+++ src/inheritance/Test1.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+
+
+public class Test1 {
+	public static void main(String[] args) {
+		Parent p =new Parent();
+		System.out.println(p.x);
+		System.out.println(p.y);
+		
+		p.m1();
+		p.m2();
+	}
+}
